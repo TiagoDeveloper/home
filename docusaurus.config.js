bbdc307b7a864863fac6c53248b7466b4fd93e92
tiagoDeveloper <tiagopereirafonseca@gmail.com>
2025-04-29 10:28:46 -0300

    VOltando config docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://tiagodeveloper.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/home',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
